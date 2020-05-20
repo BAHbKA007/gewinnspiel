bc9293885013a0add9952b080604c474b1c923dd
BAHbKA007 <peniskopfbacken@gmail.com>
2020-05-20 12:14:38 +0200

    asdasd

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return Redirect::to('http://werner-ebert.de');
+});
+
 Route::get('/da39a3ee5e6b4b0d3255bfef95601890afd80709', "TeilnehmerController@index");
 Route::post('/', "TeilnehmerController@check");
 
