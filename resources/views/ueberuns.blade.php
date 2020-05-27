20e50966a5bb43bceac421ca3de63619fe6b3ad4
BAHbKA007 <peniskopfbacken@gmail.com>
2020-05-27 14:31:03 +0200

    ueber Uns

--- resources/views/ueberuns.blade.php
+++ resources/views/ueberuns.blade.php
@@ -2,27 +2,26 @@
 <!-- Main -->
 <div id="main">
     <!-- Post -->
-    <section id="jump-action"  class="post ueber_uns_font">
+    <section id="jump-action"  class="post">
         <h2>Image</h2>
         <span class="image fit"><img src="images/pic01.jpg" alt="" /></span>
         <div class="box alt">
             <div class="row gtr-50 gtr-uniform">
-                <div class="col-4"><span class="image fit"><img src="images/pic02.jpg" alt="" /></span></div>
-                <div class="col-4"><span class="image fit"><img src="images/pic03.jpg" alt="" /></span></div>
-                <div class="col-4"><span class="image fit"><img src="images/pic04.jpg" alt="" /></span></div>
                 <!-- Break -->
-                <div class="col-4"><span class="image fit"><img src="images/pic04.jpg" alt="" /></span></div>
-                <div class="col-4"><span class="image fit"><img src="images/pic02.jpg" alt="" /></span></div>
-                <div class="col-4"><span class="image fit"><img src="images/pic03.jpg" alt="" /></span></div>
+                <div class="col-4"><span class="image fit"><img src="images/Ebert_18.jpg" alt="" /></span></div>
+                <div class="col-4"><span class="image fit"><img src="images/Ebert_17.jpg" alt="" /></span></div>
+                <div class="col-4"><span class="image fit"><img src="images/Ebert_03.jpg" alt="" /></span></div>
                 <!-- Break -->
-                <div class="col-4"><span class="image fit"><img src="images/pic03.jpg" alt="" /></span></div>
-                <div class="col-4"><span class="image fit"><img src="images/pic04.jpg" alt="" /></span></div>
-                <div class="col-4"><span class="image fit"><img src="images/pic02.jpg" alt="" /></span></div>
+                <div class="col-4"><span class="image fit"><img src="images/Ebert_15.jpg" alt="" /></span></div>
+                <div class="col-4"><span class="image fit"><img src="images/Ebert_14.jpg" alt="" /></span></div>
+                <div class="col-4"><span class="image fit"><img src="images/Ebert_13.jpg" alt="" /></span></div>
             </div>
         </div>
 
         <h1>Damals wie heute</h1>
-        <p><span class="image left"><img src="images/pic08.jpg" alt="" /></span>Lorem ipsum dolor sit accumsan interdum nisi, quis tincidunt felis sagittis eget. tempus euismod. Vestibulum ante ipsum primis in faucibus vestibulum. Blandit adipiscing eu felis iaculis volutpat ac adipiscing accumsan eu faucibus. Integer ac pellentesque praesent tincidunt felis sagittis eget. tempus euismod. Vestibulum ante ipsum primis sagittis eget. tempus euismod. Vestibulum ante ipsum primis in faucibus vestibulum. Blandit adipiscing eu felis iaculis volutpat ac adipiscing accumsan eu faucibus. Integer ac pellentesque praesent tincidunt felis sagittis eget. tempus euismod. Vestibulum ante ipsum primis in faucibus vestibulum. Blandit adipiscing eu felis iaculis volutpat ac adipiscing accumsan eu faucibus. Integer ac pellentesque praesent. Vestibulum ante ipsum primis in faucibus magna blandit adipiscing eu felis iaculis.</p>
+        <p><span class="image left"><img src="images/pic08.jpg" alt="" /></span>Selbst eine Sterne-Küche ist nur so gut wie ihr Lieferant. Deshalb sind Frische und Qualität bei uns oberstes Gebot – ob bei Obst, Gemüse, exotischen Südfrüchten oder Trockenfrüchten.
+
+            Um diesem Anspruch das ganze Jahr über gerecht zu werden, importieren wir handverlesene Waren aus aller Welt – wenn es sein muss direkt aus den berühmten Pariser Markthallen (Rungis).</p>
         <p><span class="image right"><img src="images/pic09.jpg" alt="" /></span>Lorem ipsum dolor sit accumsan interdum nisi, quis tincidunt felis sagittis eget. tempus euismod. Vestibulum ante ipsum primis in faucibus vestibulum. Blandit adipiscing eu felis iaculis volutpat ac adipiscing accumsan eu faucibus. Integer ac pellentesque praesent tincidunt felis sagittis eget. tempus euismod. Vestibulum ante ipsum primis sagittis eget. tempus euismod. Vestibulum ante ipsum primis in faucibus vestibulum. Blandit adipiscing eu felis iaculis volutpat ac adipiscing accumsan eu faucibus. Integer ac pellentesque praesent tincidunt felis sagittis eget. tempus euismod. Vestibulum ante ipsum primis in faucibus vestibulum. Blandit adipiscing eu felis iaculis volutpat ac adipiscing accumsan eu faucibus. Integer ac pellentesque praesent. Vestibulum ante ipsum primis in faucibus magna blandit adipiscing eu felis iaculis.</p>
 
     </section>
