5620330e6ccff9dde7106b7140d1a127d19284fc
BAHbKA007 <peniskopfbacken@gmail.com>
2020-05-29 13:45:50 +0200

    Bestimmte dinger

--- resources/views/mail/after_confirm.blade.php
+++ resources/views/mail/after_confirm.blade.php
@@ -9,17 +9,25 @@
                 E-Mail bestätigt
             </h1>
 
-            <h2>Vielen Dank für Deine Teilnahme. Wir wünschen Dir viel Erfolg.</h2>
+            <h2>Vielen Dank für Deine Teilnahme. <br> Wir wünschen Dir viel Erfolg.</h2>
             <p>
-                Jetzt vormerken, die Verlosung erfolgt am 8.6 ab 17 Uhr als Live-Videostream unter <a href="https://www.facebook.com/werner.ebert.stgt" target="_blank">facebook.com/werner.bert.stgt</a> und <a href="https://www.instagram.com/werner.ebert.stgt" target="_blank">instagram.com/werner.bert.stgt</a>
+                Jetzt vormerken, die Verlosung erfolgt am 8.6 ab 17 Uhr als Live-Videostream unter 
+                <a href="https://www.facebook.com/werner.ebert.stgt" class="icon brands fa-facebook-f" target="_blank"><span class="label">Facebook</span> Facebook</a> & 
+                <a href="https://www.instagram.com/werner.ebert.stgt/" class="icon brands fa-instagram" target="_blank"><span class="label">Instagram</span> Instagram</a>.
             </p>
             <p>
                 Wenn Du gewinnst benachrichtigen wir Dich über Deine angegebene E-Mail-Adresse. Bitte prüfe ggf. Deinen Spam-Ordner. 
                 Wir bitten Dich den Empfang innerhalb von 7 Tagen, spätestens bis 17.05.2020 zu bestätigen, damit wir sicher gehen können, 
                 dass die Gewinner ihr Nachricht auch erhalten haben.
             </p>
+            <br>
             <p>
-                Mehr über uns: <a href="https://www.werner-ebert.de" target="_blank">Werner Ebert GmbH</a>
+                Mehr über uns:             
+                <ul class="icons alt">
+                    <li><a href="https://www.facebook.com/werner.ebert.stgt" class="icon brands fa-facebook-f" target="_blank"><span class="label">Facebook</span></a></li>
+                    <li><a href="https://www.instagram.com/werner.ebert.stgt/" class="icon brands fa-instagram" target="_blank"><span class="label">Instagram</span></a></li>
+                    <li><a href="https://www.werner-ebert.de/" class="icon brands alt fa-dribbble" target="_blank"><span class="label">Dribbble</span></a></li>
+                </ul>
             </p>
         @else
         <header class="major">
--- resources/views/teilnahme_erfolgreich.blade.php
+++ resources/views/teilnahme_erfolgreich.blade.php
@@ -8,7 +8,7 @@
         </header> --}}
 
         <!-- Text stuff -->
-            <h2>Vielen Dank für Deine Teilnahme. Wir wünschen Dir viel Erfolg!</h2>
+            <h2>Vielen Dank für Deine Teilnahme. <br> Wir wünschen Dir viel Erfolg!</h2>
             Bitte prüfe Deinen E-Mail-Eingang und vergiß nicht Deine Teilnahme zu bestätigt.
             <br>Sei live dabei bei der Verlosung am 8. Juni 2020 ab 17 Uhr im Videostream über unsere Social Media Kanäle <br>
             <a href="https://www.facebook.com/werner.ebert.stgt" class="icon brands fa-facebook-f" target="_blank"><span class="label">Facebook</span> Facebook</a> & 
